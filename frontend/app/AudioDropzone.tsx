22a7d142de4a9c7276c28e3bc6df43752a855b97
felixng <ngthinh302@gmail.com>
2023-05-27 17:54:50 +0700

    frontend: Add error handling for audio upload

--- frontend/app/AudioDropzone.tsx
+++ frontend/app/AudioDropzone.tsx
@@ -3,12 +3,15 @@
 import React, { useCallback, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
 
+const maxFileNameLength = 50;
+const maxFileSize = 50000000; // 50 MB
+
 export default function AudioDropZone() {
   const onDrop = useCallback((acceptedFiles: File[]) => {
     if (acceptedFiles.length === 0) {
       console.log("No files accepted");
     } else {
-      console.log(acceptedFiles);
+      console.log("Accepted file:", acceptedFiles[0]);
     }
   }, []);
 
@@ -19,6 +22,8 @@ export default function AudioDropZone() {
     isFocused,
     isDragAccept,
     isDragReject,
+    acceptedFiles,
+    fileRejections,
   } = useDropzone({
     accept: {
       "audio/wav": [".wav"],
@@ -30,7 +35,25 @@ export default function AudioDropZone() {
     },
     maxFiles: 1,
     onDrop,
+    validator: fileValidator,
   });
+
+  const acceptedFileItems = acceptedFiles.map((file) => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
+
+  const fileRejectionItems = fileRejections.map(({ file, errors }) => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes 
+      <ul>
+        {errors.map((e) => (
+          <li key={e.code}>{e.message}</li>
+        ))}
+      </ul>
+    </li>
+  ));
 
   const style = useMemo(
     () => ({
@@ -50,8 +73,26 @@ export default function AudioDropZone() {
       ) : (
         <p>Drag and drop an audio file, or click to select</p>
       )}
+      <ul>{acceptedFileItems}</ul>
+      <ul>{fileRejectionItems}</ul>
     </div>
   );
+}
+
+function fileValidator(file: File) {
+  if (file.name.length > maxFileNameLength) {
+    return {
+      code: "file-name-too-long",
+      message: `File name is too long. Maximum length is ${maxFileNameLength} characters.`,
+    };
+  }
+  if (file.size > maxFileSize) {
+    return {
+      code: "file-too-large",
+      message: `File is too large. Maximum size is ${maxFileSize} bytes.`,
+    };
+  }
+  return null;
 }
 
 const baseStyle = {
@@ -59,8 +100,8 @@ const baseStyle = {
   maxWidth: "1000px",
   height: "200px",
   display: "flex",
+  flexDirection: "column" as "column", // TypeScript is weird
   alignItems: "center",
-  justifyContent: "center",
   padding: "20px",
   borderWidth: 2,
   borderRadius: 10,
