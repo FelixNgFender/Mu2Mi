1e3a2f430baef9f98a39d550949437d107e91ddf
felixng <ngthinh302@gmail.com>
2023-05-29 22:48:07 +0700

    frontend: Refactor file upload to native

---
+++ frontend/app/AudioUploadForm.tsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+export default function AudioUploadForm() {
+  return (
+    // action should be the URL that processes the form submission
+    <form
+      className="flex flex-col gap-3 w-full max-w-screen-xl flex-1"
+      name="audio-upload-form"
+      id="audio-upload-form"
+      action=""
+      method="post"
+      encType="multipart/form-data"
+    >
+      <div className="flex items-center justify-center">
+        <label
+          htmlFor="upload"
+          className="flex flex-col items-center justify-center w-full h-64 border-2 border-dashed rounded-lg cursor-pointer bg-white hover:bg-slate-50  "
+        >
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg
+              aria-hidden="true"
+              className="w-10 h-10 mb-3 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+              ></path>
+            </svg>
+            <p className="mb-2 text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p className="hidden text-sm text-gray-500 dark:text-gray-400 sm:block">
+              MP3, WAV, FLAC, AAC, OGG, or M4A (MAX. 50MB)
+            </p>
+          </div>
+          <input
+            name="upload"
+            id="upload"
+            type="file"
+            accept=".mp3, .wav, .flac, .aac, .ogg, .m4a"
+            className="hidden"
+          />
+        </label>
+      </div>
+
+      <label htmlFor="ytb-link" className="hidden"></label>
+      <input
+        className="border-2 border-gray-300 rounded-lg p-2 w-full text-center "
+        type="url"
+        id="ytb-link"
+        name="ytb-link"
+        placeholder="Or enter a YouTube link"
+      />
+
+      <button
+        className="self-center bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+        form="audio-upload-form"
+        type="submit"
+      >
+        <svg
+          className= "mr-1"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 -960 960 960"
+          width="24"
+        >
+          <path d="M220-160q-24 0-42-18t-18-42v-143h60v143h520v-143h60v143q0 24-18 42t-42 18H220Zm230-153v-371L330-564l-43-43 193-193 193 193-43 43-120-120v371h-60Z" />
+        </svg>
+        <span>Submit</span>
+      </button>
+    </form>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,12 @@
-import AudioDropZone from "./AudioDropzone";
+import AudioUploadForm from "./AudioUploadForm";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import YoutubeUpload from "./YoutubeUpload";
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center gap-4 antialiased bg-gray-200 min-h-screen p-4">
       <Navbar></Navbar>
-      <div
-        className="flex flex-col gap-2 w-full max-w-screen-xl flex-1"
-        role="presentation"
-      >
-        <AudioDropZone ></AudioDropZone>
-        <YoutubeUpload></YoutubeUpload>
-      </div>
+      <AudioUploadForm></AudioUploadForm>
       <Footer></Footer>
     </main>
   );
