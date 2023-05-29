7bcdb918379e928fc55a1fb87ff5fc0e0d2acd39
felixng <ngthinh302@gmail.com>
2023-05-29 11:34:02 +0700

    frontend: Implement Footer component

---
+++ frontend/app/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-sm">
+      <p>Copyright © {new Date().getFullYear()} FelixNgFender</p>
+    </footer>
+  );
+}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,20 @@
 import AudioDropzone from "./AudioDropzone";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 import YoutubeUpload from "./YoutubeUpload";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center gap-4 antialiased bg-gray-200 min-h-screen p-8">
+    <main className="flex flex-col items-center gap-4 antialiased bg-gray-200 min-h-screen p-4">
       <Navbar></Navbar>
       <div
-        className="flex flex-col gap-2 w-full max-w-screen-xl"
+        className="flex flex-col gap-2 w-full max-w-screen-xl flex-1"
         role="presentation"
       >
         <AudioDropzone></AudioDropzone>
         <YoutubeUpload></YoutubeUpload>
       </div>
+      <Footer></Footer>
     </main>
   );
 }
