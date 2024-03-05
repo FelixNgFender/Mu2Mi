e285d552f86661d038eaac58a21a7cd961ff1ba1
felixng <ngthinh302@gmail.com>
2024-03-04 20:21:19 -0500

    fix: error handling when updating profile

--- src/app/settings/actions.ts
+++ src/app/settings/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { siteConfig } from '@/config/site';
+import { AppError } from '@/lib/error';
+import { httpStatus } from '@/lib/http';
 import { authAction } from '@/lib/safe-action';
 import { updateOne as updateOneUser } from '@/models/user';
 import { revalidatePath } from 'next/cache';
@@ -10,6 +12,15 @@ import { userFormSchema } from './schemas';
 const schema = userFormSchema;
 
 export const editProfile = authAction(schema, async (newUser, { user }) => {
-    await updateOneUser(user.id, newUser);
+    try {
+        await updateOneUser(user.id, newUser);
+    } catch (error) {
+        throw new AppError(
+            'HttpError',
+            httpStatus.clientError.badRequest.humanMessage,
+            true,
+            httpStatus.clientError.badRequest.code,
+        );
+    }
     revalidatePath(siteConfig.paths.settings);
 });
