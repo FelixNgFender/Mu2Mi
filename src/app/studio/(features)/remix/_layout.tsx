818f368a2a28cfa797bc4a5e8f9b73785300d80a
felixngfender <ngthinh302@gmail.com>
2024-11-01 22:10:58 -0400

    feat: reduce operational cost, why does next keep doing this to me?

---
+++ src/app/studio/(features)/remix/_layout.tsx
@@ -0,0 +1,13 @@
+// type RemixLayoutProps = {
+//     children: React.ReactNode;
+// };
+//
+// const RemixLayout = ({ children }: RemixLayoutProps) => {
+//     return (
+//         <section className="container relative flex h-full max-w-screen-lg flex-col space-y-4 py-4">
+//             {children}
+//         </section>
+//     );
+// };
+//
+// export default RemixLayout;
